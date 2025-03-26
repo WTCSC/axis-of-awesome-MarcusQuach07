c00c2ed4724e97384efbc7dfbb73d9a114406138
Marcus Quach <marcusquach07@gmail.com>
2025-03-26 15:35:28 -0600

    Not done

--- Code.py
+++ Code.py
@@ -1,15 +1,23 @@
 import pandas as pd
+from pandas_datareader import data as pdr
 import matplotlib as plt
-import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import style
+import datetime as dt
 
-x, y = np.loadtxt('Dataset.csv',
-                  unpack=True,
-                  delimiter=',')
+# Taking in data from 2020, January 1st
+start = dt.datetime(2020, 1, 1)
 
-plt.plot(x,y)
+# Taking in data until 2025, March 26th
+end = dt.datetime(2025, 3, 26)
+# Variable called Nvidia taking in pandas datareader and using yahoo to search from start to end
+Nvidia = pdr.DataReader('NVDA', 'yahoo', start, end)
 
-plt.title('Value of Gold From 1833 to 2024')
-plt.xlabel('Years')
-plt.ylabel('Value in USD')
+# Line graph style
+style.use('ggplot')
 
+# Plotting nvdias close price
+Nvidia['Close'].plot(figsize=(8,8), label = 'Nvidia')
+
+# Show line graph
 plt.show()
