5a89493bb0b652ca9d99463719d5acfb7839bed1
sh09unn3r <marcusquach07@gmail.com>
2025-03-27 09:57:52 -0600

    Testing

--- Code.py
+++ Code.py
@@ -10,14 +10,24 @@ start = dt.datetime(2020, 1, 1)
 
 # Taking in data until 2025, March 26th
 end = dt.datetime(2025, 3, 26)
+
 # Variable called Nvidia taking in pandas datareader and using yahoo to search from start to end
 Nvidia = pdr.DataReader('NVDA', 'yahoo', start, end)
+AMD = pdr.DataReader('AMD', 'yahoo', start, end)
 
 # Line graph style
 style.use('ggplot')
 
 # Plotting nvdias close price
 Nvidia['Close'].plot(figsize=(8,8), label = 'Nvidia')
+AMD['Close'].plot(figsize=(8,8), label = 'AMD')
+
+plt.title('Nvidia vs AMD')
+
+plt.legend(loc='lower right')
+
+plt.xlabel('Date', fontsize=15)
+plt.ylabel('Price in USD', fontsize=15)
 
 # Show line graph
 plt.show()
